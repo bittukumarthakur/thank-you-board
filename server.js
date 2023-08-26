ef3e27989c5ce5ddbbd9b3d9ec53265a32d856eb
Bittu <bittukumarthakur@users.noreply.github.com>
2023-08-26 11:07:43 +0530

    initial commit.

---
+++ server.js
@@ -0,0 +1,5 @@
+const main = () => {
+  console.log("THANK YOU BOARD APP...")
+}
+
+main();
---
+++ test/models/user-test.js
@@ -0,0 +1,9 @@
+const { it, describe } = require("node:test");
+const { strictEqual } = require("assert");
+
+describe("user", () => {
+  it("message", () => {
+    strictEqual(1, 1);
+  });
+
+});
